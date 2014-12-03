9a0c4423ccff3d9796799721a9f00a426ad34d6e
rob wilke <rob@justdothis.com>
2014-12-03 11:50:05 -0500

    added devise gem for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root to: "welcome#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
